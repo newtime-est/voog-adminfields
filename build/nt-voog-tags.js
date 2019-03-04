699df7a4ad53f06ff08cf78bdd7d90b1bb8adcfe
ants <ants@newtime.ee>
2019-03-04 22:59:45 +0200

    inital custom class with basic methods.

--- build/nt-voog-tags.js
+++ build/nt-voog-tags.js
@@ -2,10 +2,37 @@
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Rectangle = function Rectangle(height, width) {
-  _classCallCheck(this, Rectangle);
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-  this.height = height;
-  this.width = width;
-  console.log("whaay");
-};
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var TagsGroup =
+/*#__PURE__*/
+function () {
+  function TagsGroup(options) {
+    _classCallCheck(this, TagsGroup);
+
+    this.pageData = options.pageData;
+    this.recievedGroupsDOM = document.querySelectorAll('.ntjs-tags-area');
+    this.groups = [];
+    this.init();
+  }
+
+  _createClass(TagsGroup, [{
+    key: "init",
+    value: function init() {
+      this.recieveGroups();
+    }
+  }, {
+    key: "recieveGroups",
+    value: function recieveGroups() {
+      var groups = [];
+      this.recievedGroupsDOM.forEach(function (item) {
+        groups.push(item.getAttribute('data-group'));
+      });
+      this.groups = groups;
+    }
+  }]);
+
+  return TagsGroup;
+}();
--- src/js/nt-voog-tags.js
+++ src/js/nt-voog-tags.js
@@ -1,7 +1,24 @@
-class Rectangle {
-    constructor(height, width) {
-      this.height = height;
-      this.width = width;
-      console.log("whaay");
+class TagsGroup {
+    constructor(options) {
+      this.pageData = options.pageData;
+      this.recievedGroupsDOM = document.querySelectorAll('.ntjs-tags-area');
+      this.groups = [];
+      this.init();
+    }
+    
+    init() {
+        this.recieveGroups();
+        
+    }
+
+    recieveGroups() {
+        const groups = [];
+
+        this.recievedGroupsDOM.forEach(function (item) {
+            groups.push(item.getAttribute('data-group'));
+        });
+
+        this.groups = groups;
     }
   }
+  
