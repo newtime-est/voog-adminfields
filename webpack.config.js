b417d971ae0de694d99649af2ef5b63af0fd62e5
Kozjat <arvo.saalits@gmail.com>
2019-03-05 19:24:15 +0200

    Added webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+// const webpack = require('webpack');
+
 module.exports = {
-    entry: './src/js/nt-voog-tags.js',
+    entry: './src/nt-voog-tags.js',
+    mode: 'production',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
     },
     module: {
         rules: [
-          { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
         ]
     },
     stats: {
